83dc3d5032344d42b67e95124ab4ea63d76b79fa
Bill_Zhang <zhx79513@gmail.com>
2013-07-22 16:28:42 +0800

    modification test

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -14,6 +14,7 @@ namespace GitTest
 		[STAThread]
 		static void Main()
 		{
+			// test test test
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
