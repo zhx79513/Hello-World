3d75a725dd26f7f04344f3afb00d7704d043b9d2
zhx79513 <zhx79513@gmail.com>
2013-07-23 17:13:13 +0800

    test

--- GitTest/Form1.Designer.cs
+++ GitTest/Form1.Designer.cs
@@ -28,13 +28,28 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
 			this.SuspendLayout();
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+			this.comboBox1.Location = new System.Drawing.Point(59, 71);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(171, 20);
+			this.comboBox1.TabIndex = 0;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(384, 262);
+			this.Controls.Add(this.comboBox1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
@@ -42,6 +57,8 @@
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ComboBox comboBox1;
 	}
 }
 
